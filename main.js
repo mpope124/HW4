51b83c80377ae683d11f57d0bc355906afc69113
mpope124 <mpope124@gmail.com>
2023-01-27 14:22:13 -0800

    Check for errors but this should be it for the HW

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var Excercise = function(pWhich, pLength, pCalories){
+let Exercise = function(pWhich, pLength, pCalories){
    this.which = pWhich;
    this.length = pLength;
    this.calories = pCalories;
@@ -11,32 +11,50 @@ workArr = [];
 
 
 function Workout() {
-   var minutes = document.getElementById("minutes").value;
-   var sitUps = document.getElementById("sitUp");
-   var pushUps = document.getElementById("pushUp");
-   var jumpRopes = document.getElementById("jumpRope");
+   let minutes = document.getElementById("minutes").value;
+   let sitUps = document.getElementById("sitUp");
+   let pushUps = document.getElementById("pushUp");
+   let jumpRopes = document.getElementById("jumpRope");
 
    // These make sure which exercise the values get pushed into the array.
    if(sitUps.checked==true){
-      workArr.push(new Excercise("Sit Ups", minutes, 10 * minutes));
+      workArr.push(new Exercise("Sit Ups", minutes, 10 * minutes));
       console.log(workArr);
    }
    else if(pushUps.checked==true){
-      workArr.push(new Excercise("Push Ups", minutes, 15 * minutes));
+      workArr.push(new Exercise("Push Ups", minutes, 15 * minutes));
       console.log(workArr);
    }
    else if(jumpRopes.checked==true){
-      workArr.push(new Excercise("Jump Rope", minutes, 18 * minutes));
+      workArr.push(new Exercise("Jump Rope", minutes, 18 * minutes));
       console.log(workArr);
    }
 } // Onclick Button for add
 
+document.getElementById('mostBurned').addEventListener('click', mostBurned);
+
+function mostBurned() {
+    let winner = 0
+    for (i = 0; i < workArr.length; i++) {
+    if  (workArr[i].calories > workArr[winner].calories);
+    {
+        winner = i;
+    }
+    }
+    document.getElementById("outputMostBurned").value = workArr[winner].which + ' ' + workArr[winner].calories;
+}
 document.addEventListener("DOMContentLoaded", function (event) {
 });
 
+document.getElementById('showAll').addEventListener('click', showAll);
 
-    //let workout = {
-     //  which: "",
-     //  length: "", //The professor adds the length in his video. We may not need this one.
-     //  calories: "",  
-    //};
+function showAll(){
+    document.getElementById('outputAll').innerText = '';
+    for (i = 0; i < workArr.length; i++) {
+        let list = document.createElement('li');
+        list.innerText = workArr[i].which + ' for ' + workArr[i].length + ' minutes' + ' and burned ' + workArr[i].calories + ' calories';
+        document.getElementById('outputAll').append(list);
+        console.log(workArr[i]);
+    }
+    
+}
